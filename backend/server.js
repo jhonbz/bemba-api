731913527beff93f9b65e2d46585bbdff980c5f0
Jhon <jjbonzu@hotmail.com>
2025-03-24 22:58:55 -0500

    ordenando todo

--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,13 @@ const mysql = require('mysql2');
 const cors = require("cors");
 const authRoutes = require("./routes/auth_routes");
 const sessionRoutes = require("./routes/session_routes");
+const userRoutes = require("./routes/user_routes");
 
 const app = express();
 
-
+// Middleware
 app.use(express.json());
 app.use(cors());
-app.use(require('cors')());
-app.use("/api/sessions", sessionRoutes);
 
 // Configurar conexión con AWS RDS
 const db = mysql.createConnection({
@@ -25,21 +24,19 @@ const db = mysql.createConnection({
 db.connect(err => {
     if (err) {
         console.error('Error al conectar a la base de datos:', err);
-        return;
+        process.exit(1); // Detiene el servidor si no hay conexión
     }
     console.log('Conectado a AWS RDS');
 });
 
-// Ruta de prueba
-app.get('/', (req, res) => {
-    res.send('¡Bienvenido a la API de BEMBA!');
-});
-
+// Rutas
+app.get('/', (req, res) => res.send('¡Bienvenido a la API de BEMBA!'));
 app.use("/api/auth", authRoutes);
-app.use("/api/users", require("./routes/user_routes"));
+app.use("/api/users", userRoutes);
+app.use("/api/sessions", sessionRoutes);
+
 // Iniciar el servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
-
